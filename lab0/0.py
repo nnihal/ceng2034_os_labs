347a1411b2f4e3b1b3d722aebae94e46503bd991
U160709020 <ilaydanihalarikan@posta.mu.edu.tr>
2019-03-04 17:59:20 +0300

    lab0

--- lab0/0.py
+++ lab0/0.py
@@ -2,29 +2,21 @@ import os
 
 """
 myfolder = os.getcwd()
-
 print(os.system("clear"))
 print(myfolder)
-
 print(os.listdir())
-
 print("second index", os.listdir()[1])
-
 #print("_______________________________")
 #print("ls -la ", os.system('ls -la'))
-
 print("is it directory? ",os.path.isdir(myfolder))
-
 mypath = os.path.join(myfolder , "another")
-
 print(mypath)
-
 print("is it dir ?", os.path.isdir(mypath))
 """
 
 os.chdir(os.getenv("HOME"))
 os.mkdir("os_lab_0")
 os.chdir("os_lab_0")
-os.system("touch 2.txt 1.py")
+os.system("touch ilayda.txt nihal.txt 1.py")
 print (os.system("ls -lt"))
 print (os.system("ls *txt"))
